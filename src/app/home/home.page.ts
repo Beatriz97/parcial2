4591a15ea6959ae1c5b9e810c17eccaf0acab590
Beatriz97 <sbhg0704@gmail.com>
2020-04-19 23:00:31 -0600

    adiccionando proyecto

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; 
+import { MediaCapture, MediaFile } from '@ionic-native/media-capture/ngx';
+import { SocialSharing} from '@ionic-native/social-sharing/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,23 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(public mediaCapture:MediaCapture, 
+              public socialSharing:SocialSharing) {}
 
+ CaptureAudio(){
+   this.mediaCapture.captureAudio().then((audio:MediaFile[])=>{
+     this.ShareMedia("audio file capture by media capture plugin", "media capture", audio[0].fullPath,"");
+   }, (err)=>{
+     alert(JSON.stringify(err));
+   })
+ }             
+
+ ShareMedia(message,subject,filepath,url){
+   this.socialSharing.share(message,subject,filepath,url).then(()=>{
+
+   },(err)=>{
+     alert(JSON.stringify(err));
+   });
+ }
+ 
 }
